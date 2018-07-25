a011f6b80d11b813775f1ef2a3fc1176dbf9eb3d
pfistfl <pfistererf@googlemail.com>
2018-07-25 13:17:15 +0200

    Load after installing

--- R/redisServer.R
+++ R/redisServer.R
@@ -2,6 +2,7 @@ library(mlr)
 library(foreach)
 library(doRedis)
 library(agent)
+library(processx)
 
 # Set up redis server password: --------------------------------------------------------------------
 # agent::agent_set("redispass", "some_password")
@@ -13,9 +14,8 @@ setProgress(TRUE)         # Progress bar
 registerDoRedis("jobs", host = "10.153.53.62") # , password = agent::agent_get("redispass"))
 
 # Start some local workers
-startLocalWorkers (n = 2, queue = "jobs", host = "10.153.53.62") # , password = agent::agent_get("redispass"))
+ps = startLocalWorkers (n = 2, queue = "jobs", host = "10.153.53.62") # , password = agent::agent_get("redispass"))
 
-getDoParWorkers()
 
 # Define the Jobs: ---------------------------------------------------------------------------------
 tsks = list(iris.task, pid.task)
@@ -23,9 +23,11 @@ lrns = list(makeLearner("classif.rpart"), makeLearner("classif.xgboost"))
 
 ff = foreach(icount(100)) %:%
   foreach(tsk = tsks, .combine = "c") %:%
-    foreach(lrn = lrns) %dopar% {
+    foreach(lrn = lrns, .packages = "mlr") %dopar% {
       res = resample(lrn, tsk, cv3, show.info = TRUE)
     }
 
 # Stop the Jobs: -----------------------------------------------------------------------------------
+killAllWorkers(ps)
 removeQueue("jobs")
+
--- R/redisSlave.R
+++ R/redisSlave.R
@@ -1,15 +1,15 @@
 # Install / Load Packages as required
 if (!require("devtools"))
-  install.packages("devtools")
+  install.packages("devtools"); library("devtools")
 
 if (!require("foreach"))
-  install.packages("foreach")
+  install.packages("foreach"); library("foreach")
 
 if (!require("doRedis"))
-  devtools::install_github("bwlewis/doRedis")
+  devtools::install_github("bwlewis/doRedis"); library("doRedis")
 
 if (!require("processx"))
-  devtools::install_github("r-lib/processx")
+  devtools::install_github("r-lib/processx"); library(processx)
 
 # if (!require("agent"))
 #   devtools::install_github("ropensci/agent")
